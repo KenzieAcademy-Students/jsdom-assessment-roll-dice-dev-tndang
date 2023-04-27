d8f7154dc23cf8d27bb577a90e733d585609e464
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-04-27 02:20:30 -0400

    Completed step 1

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Complete your work inside of `code.js`
 
 Follow the instructions provided on my.kenzie.academy for this assignment.
+
+Refered to the following as reference:
+
+- Week 9 notes on Loops
+- Week 14 notes on Callback Functions and the Math Library
+- https://www.w3schools.com/js/js_random.asp
--- code.js
+++ code.js
@@ -1,4 +1,24 @@
 let results = [null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Your Code Here!
+let rollDie = function() {
+    let result = Math.floor(Math.random() * 6) + 1;
+    console.log(`You rolled a ${result}`);
+    return result;
+}
+
+let pairResult = function() {
+    let sum = 0;
+    for (let roll = 1; roll <= 2; roll++) {
+        sum += rollDie();
+    }
+    let finalResult = document.getElementById("result");
+    finalResult.innerText = `You rolled a ${sum}!`;
+    console.log(`The final result of your roll is ${sum}!`);
+    return sum;
+}
+
+let roll = document.getElementById("roll");
+roll.addEventListener("click", pairResult)
+
 
